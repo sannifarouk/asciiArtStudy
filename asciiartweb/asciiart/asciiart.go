b93c7c579e3d85d0f056491e1db79bca97642b40
farosanni <farouksanni2002@gmail.com>
2026-04-16 17:25:47 +0100

    work in progress

---
+++ asciiartweb/asciiart/asciiart.go
@@ -0,0 +1,55 @@
+package asciiart
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)	
+
+func AsciiArt(text string, banner string) string {
+
+	bannerFile, err := os.ReadFile("asciiart/" + banner + ".txt")
+	if err != nil {
+		fmt.Println("Error!", err)
+		return ""
+	}
+
+	splitBanner := strings.Split(string(bannerFile), "\n")
+	splitInput := strings.Split(text, "\\n")
+	
+	output := ""
+
+	for _, word := range splitInput {
+
+		for i := 1; i <= 8; i++ {
+
+			for _, char := range word {
+				output += (splitBanner[((int(char) - 32) * 9) + i])
+			}
+			output += "\n"
+		}
+	}
+
+	return output
+
+}
+
+
+	// args := os.Args
+	// if len(args) < 2 {
+	// 	fmt.Println("Error! Enter at least a letter.")
+	// 	return ""
+	// }
+
+	// bannerType := banner
+	// // if len(args) < 3 {
+	// 	bannerType = "standard.txt"
+	// 	// fmt.Println("The Art was printed with 'standard' banner. Kindly input the banner of choice if you wish to change this" )
+	// } else {
+	// 	bannerType = args[2]
+	// }
+
+	// if args[1] == "" {
+	// 	fmt.Print("")
+	// 	return ""
+	// }
--- asciiartweb/main.go
+++ asciiartweb/main.go
@@ -4,20 +4,60 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"html/template"
+	"asciiartweb/asciiart"
+	
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the Ascii-art page!")
+	// fmt.Fprintln(w, "Welcome to the Ascii-art page!")
+	
+	tmpl, err := template.ParseFiles("templates/asciiart.html")
+	
+	if err != nil {
+		fmt.Println("Error!", err)
+		return 
+	}
+
+	data := struct {
+		Text string
+		Banner string
+	} {
+		Text: "hello",
+		Banner: "shadow",
+	}
+
+	tmpl.Execute(w, data)
 
 }
 
-func AsciiArt(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Welcome to the home page!")
+func AsciiArtWeb(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	text := r.FormValue("text")
+	banner := r.FormValue("banner")
+	result := asciiart.AsciiArt(text, banner)
+
+
+	tmpl, err := template.ParseFiles("templates/result.html")
+	
+	if err != nil {
+		fmt.Println("Error!", err)
+		return 
+	}
+
+	data := struct {
+		Result string
+	} {
+		Result: result,
+	}
+
+	tmpl.Execute(w, data)
+	
 }
 
 func main() {
 	http.HandleFunc("/", Home)
-	http.HandleFunc("/ascii-art", AsciiArt)
+	http.HandleFunc("/ascii-art", AsciiArtWeb)
 	log.Println("http.//localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))	
 
